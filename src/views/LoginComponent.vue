a2b0e4f657c3b5a18c9a61cff89ab9785568e751
unknown <seevaaluu@gmail.com>
2022-12-02 15:48:40 -0600

    login

--- src/views/LoginComponent.vue
+++ src/views/LoginComponent.vue
@@ -18,6 +18,13 @@
             </v-card>
           </v-layout>
         </v-container>
+        <div class="custom-shape-divider-bottom-1670016865">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path
+              d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+              class="shape-fill"></path>
+          </svg>
+        </div>
       </v-col>
       <v-col cols="12" md="6">
         <v-container fluid fill-height>
@@ -59,4 +66,24 @@
     background-color: #0190ff;
     height: 102vh;
   }
+    .custom-shape-divider-bottom-1670016865 {
+      position: absolute;
+      bottom: -1%;
+      left: 0;
+      width: 52%;
+      overflow: hidden;
+      line-height: 0;
+      transform: rotate(180deg);
+    }
+  
+    .custom-shape-divider-bottom-1670016865 svg {
+      position: relative;
+      display: block;
+      width: calc(282% + 1.3px);
+      height: 238px;
+    }
+  
+    .custom-shape-divider-bottom-1670016865 .shape-fill {
+      fill: #FFFFFF;
+    }
 </style>

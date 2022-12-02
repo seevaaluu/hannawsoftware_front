98a0a7a2475f74758678af0af4985526787d9c48
unknown <seevaaluu@gmail.com>
2022-12-01 19:41:38 -0600

    inital configs

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import Vuetify from "./plugins/vuetify.js";
+import routes from "./routes/routes.js";
+
+Vue.use(VueRouter);
+
+import VueRouter from "vue-router";
+
+const router = new VueRouter({
+  routes,
+  mode: "history",
+});
+
 new Vue({
+  router,
+  vuetify: Vuetify,
   render: h => h(App),
 }).$mount('#app')

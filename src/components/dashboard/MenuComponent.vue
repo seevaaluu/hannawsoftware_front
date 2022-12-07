b7f9bd98e2395738a991e0e2d17a4a1ddf56388d
unknown <seevaaluu@gmail.com>
2022-12-07 09:55:28 -0600

    adding logout

--- src/components/dashboard/MenuComponent.vue
+++ src/components/dashboard/MenuComponent.vue
@@ -2,14 +2,26 @@
   <v-toolbar class="elevation-2">
     <v-toolbar-title>Dashboard</v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-btn icon>
+    <v-btn icon @click="logout()">
       <v-icon>logout</v-icon>
     </v-btn>
   </v-toolbar>
 </template>
 
 <script>
+  import { errorHandling, getApiHost } from '@/lib/utilities';
+import axios from 'axios';
+
   export default {
+    methods: {
+      logout: function() {
+        axios.post(`${getApiHost()}/api/logout`)
+        .then(() => {
+          this.$router.push({name: 'login'})
+        })
+        .catch(error => errorHandling(error));
+      }
+    },
     created() {
       if(!this.$store.state.auth) {
         this.$router.push({ name: 'login' })
